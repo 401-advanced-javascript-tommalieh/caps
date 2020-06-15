8885ab1570bf8242ebfb567414ad4b3cda768ba9
Tommalieh <muhammedtommalieh@gmail.com>
2020-06-15 22:06:16 +0300

    added tcp

--- caps.js
+++ caps.js
@@ -1,20 +1,73 @@
 'use strict';
 
-const events = require('./events.js');
-events.on('pickup', (order) => logEvent('pickup', order));
-events.on('in-transit', (order) => logEvent('in-transit', order));
-events.on('delivered', (order) => logEvent('delivered', order));
+require('dotenv').config();
 
-function logEvent(event, order){
-    const eventData = {
-        event: event,
-        time: new Date(),
-        payload:{order}
-    }
-    console.log('Event', eventData);
+const net = require('net');
+const uuidv4 = require('uuid').v4;
+
+const PORT = process.env.PORT || 3000;
+const server = net.createServer();
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
+
+const socketPool = {};
+
+server.on('connection', (socket) => {
+    const socketID = `socket-${uuidv4()}`;
+    socketPool[socketID] = socket;
+    socket.on('data', (buffer) => dispatchEvent(buffer));
+
+    socket.on('error', (err) => {
+        console.log(`socket error ${err.message}`);
+    });
+
+    socket.on('end', (err) => {
+        delete socketPool[socketID];
+    })
+});
+
+function dispatchEvent(buffer){
+    // console.log('Buffer', JSON.parse(buffer));
+    const message = JSON.parse(buffer.toString().trim());
+    if(message.event && message.payload){
+    broadCast(message);
+}
 }
 
-require('./vendor.js');
-require('./driver.js');
+function broadCast(message){
+    console.log('message', message);
+    const payload = JSON.stringify(message);
+    // console.log('payload', payload);
+    for(let socket in socketPool){
+        socketPool[socket].write(payload);
+    }
+}
+
+server.on('error', (err) =>{
+    console.log(`server error`, err.message)
+})
+
+
+
+
+
+// const events = require('./events.js');
+// events.on('pickup', (order) => logEvent('pickup', order));
+// events.on('in-transit', (order) => logEvent('in-transit', order));
+// events.on('delivered', (order) => logEvent('delivered', order));
+
+// function logEvent(event, order){
+//     const eventData = {
+//         event: event,
+//         time: new Date(),
+//         payload:{order}
+//     }
+//     console.log('Event', eventData);
+// }
+
+// require('./vendor.js');
+// require('./driver.js');
 
 
--- vendor.js
+++ vendor.js
@@ -1,19 +1,62 @@
 'use strict';
 
 require('dotenv').config();
-const events = require('./events.js');
+// const events = require('./events.js');
 const faker = require('faker');
+const net = require('net');
 
-events.on('delivered', (order) => console.log(`VENDOR: Thank you for delivering ${order.orderId}`));
+const client = new net.Socket();
 
-setInterval(function() {
-    
+const HOST = process.env.HOST;
+const PORT = process.env.PORT;
+
+client.connect(PORT, HOST, () => {
+    console.log('connected to server from vendor');
+
+    client.on('data', (serverData) => {
+        // console.log('What we got from the server', serverData, JSON.parse(serverData));
+        const parsedServerData = JSON.parse(serverData);
+        // console.log(`vendor parsed ${parsedServerData}`);
+        if(parsedServerData.event === 'delivered'){
+            console.log(`thank you for delivering ${parsedServerData.payload.orderId}`);
+        }
+
+    });
+});
+
+setInterval(createOrder, 5000);
+
+function createOrder(){
+
     const order = {
         storeName: process.env.STORE_NAME,
         orderId: faker.random.uuid(),
         customerName: faker.name.findName(),
         address: faker.address.city()
     }
-    events.emit('pickup', order);
+    const message = JSON.stringify({event: "pickup", time:new Date(), payload: order});
+    // console.log(message);
+    client.write(message);
+}
 
-}, 5000);
+
+
+
+
+
+
+
+
+// events.on('delivered', (order) => console.log(`VENDOR: Thank you for delivering ${order.orderId}`));
+
+// setInterval(function() {
+    
+//     const order = {
+//         storeName: process.env.STORE_NAME,
+//         orderId: faker.random.uuid(),
+//         customerName: faker.name.findName(),
+//         address: faker.address.city()
+//     }
+//     events.emit('pickup', order);
+
+// }, 5000);
